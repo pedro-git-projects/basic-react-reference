4757e02aca73dc6ac2ff59c43e8fc35a859a897b
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-23 21:50:50 -0300

    states

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,13 +1,8 @@
 import React, { Component, Fragment } from 'react';
 
 export default class AppContent extends Component {
-
-	// a react constructor always recieves a single parameter called props
-	// note that refs should be used the least as possible
-	constructor(props) {
-		super(props)
-		this.listRef = React.createRef()
-	}
+	// defined a single key in the state
+	state = {posts: []}	
 
 	anotherFunction = () => {
 		console.log("Another funcion")
@@ -21,19 +16,13 @@ export default class AppContent extends Component {
 		fetch("https://jsonplaceholder.typicode.com/posts")
 			.then((response) => response.json())
 			.then(json => {
-				console.log(json)
-//				let posts = document.getElementById("post-list")
-				const posts = this.listRef.current // the current keyword is needed
-				
-				// the arrow syntax grants us access to the this of the class, being thus the prefered method
-				this.anotherFunction()
-
-				json.forEach(function(obj) {
-					let li = document.createElement("li")
-					li.appendChild(document.createTextNode(obj.title))
-					posts.appendChild(li)
-				})
+				this.setState({posts: json})
 			})
+	}
+
+	// function that takes one argument and writes to the console
+	clickedItem = (x) => {
+		console.log("clicked", x)
 	}
 
 	render() {
@@ -43,8 +32,17 @@ export default class AppContent extends Component {
 				<hr />
 				<button onClick={this.fetchList} className="btn btn-primary" href="#">Fetch Data</button>
 				<hr/>
-				{/*this makes so we can refer the virtual DOM without using ids*/}
-				<ul id="post-list" ref={this.listRef}></ul>
+				<p>Posts is {this.state.posts.length} items long</p>
+				<ul>
+					{/*iterating over posts with the js map*/}
+					{this.state.posts.map((c) =>(
+						<li key={c.id}>
+							<a href="#!" onClick={() => this.clickedItem(c.id)}>
+								{c.title}
+							</a>
+							</li>
+					))}
+				</ul>
 			</Fragment>	
 		)
 	}
