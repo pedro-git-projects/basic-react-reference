c8f4df9057c0fc3f25b7359a2d14cf92c6f97f64
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-23 21:29:23 -0300

    more react events

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -1,12 +1,24 @@
 import React, { Component, Fragment } from 'react';
 
 export default class AppContent extends Component {
+
+	anotherFunction = () => {
+		console.log("Another funcion")
+	}
+
+	leftParagraph = () => {
+		console.log("left the paragraph")
+	}
+
 	fetchList = () => {
 		fetch("https://jsonplaceholder.typicode.com/posts")
 			.then((response) => response.json())
 			.then(json => {
 				console.log(json)
 				let posts = document.getElementById("post-list")
+				
+				// the arrow syntax grants us access to the this of the class, being thus the prefered method
+				this.anotherFunction()
 
 				json.forEach(function(obj) {
 					let li = document.createElement("li")
@@ -19,6 +31,8 @@ export default class AppContent extends Component {
 	render() {
 		return (
 			<Fragment>				
+				<p onMouseEnter = {this.anotherFunction} onMouseLeave={this.leftParagraph}> This is some text </p>
+				<hr />
 				<button onClick={this.fetchList} className="btn btn-primary" href="#">Fetch Data</button>
 				<hr/>
 				<ul id="post-list"></ul>
